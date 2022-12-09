a11b7f9b5d31597b85ecb599b613f8fa201cb449
Lee Lacey <leelacey128@gmail.com>
2022-12-09 17:27:36 -0600

    Adds POST and GET endpoints

--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -1,11 +1,12 @@
 package com.example.demo.dao;
 
 import com.example.demo.model.Person;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-
+@Repository("fakeDao")
 public class FakePersonDataAccessService implements PersonDao {
 
     private static List<Person> DB = new ArrayList<>();
@@ -16,4 +17,12 @@ public class FakePersonDataAccessService implements PersonDao {
         DB.add(new Person(id, person.getName()));
         return 1;
     }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return DB;
+    }
+
+
 }
+
